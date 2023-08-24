801da1f9402c684b260570f4e1404f663557b599
alejandrxmxnt <amont9483@gmail.com>
2023-08-24 16:17:38 -0400

    archivos php

--- forms/contact.php
+++ forms/contact.php
@@ -11,7 +11,7 @@
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
-  } else {
+  } else { 
     die( 'Unable to load the "PHP Email Form" Library!');
   }
 
